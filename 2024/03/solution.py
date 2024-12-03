bb31161c80ed2d02e450e98ea94e1e277ea689d5
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-12-03 14:56:29 -0400

    No need for defs

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -3,30 +3,26 @@ import re
 
 data = get_data(day=3, year=2024)
 
-def part_one():
-	matches = re.findall(r"mul\((\d+),(\d+)\)", data, re.MULTILINE)
-	total = 0
-	for match in matches:
-		total += int(match[0]) * int(match[1])
-	print(total)
+# Part one
+matches = re.findall(r"mul\((\d+),(\d+)\)", data, re.MULTILINE)
+total = 0
+for match in matches:
+	total += int(match[0]) * int(match[1])
+print(total)
 
-def part_two():
-	matches = re.finditer(r"mul\((\d+),(\d+)\)", data, re.MULTILINE)
-	total = 0
-	for match in matches:
-		start = match.start()
-		do_match = data.rfind("do()", 0, start)
-		dont_match = data.rfind("don't()", 0, start)
-
-		if do_match == dont_match == -1: # Neither
-			total += int(match.group(1)) * int(match.group(2))
-			continue
-		if do_match > dont_match: # Do
-			total += int(match.group(1)) * int(match.group(2))
-			continue
-		else: # Don't
-			continue
-	print(total)
-
-part_one()
-part_two()
+# Part two
+matches = re.finditer(r"mul\((\d+),(\d+)\)", data, re.MULTILINE)
+total = 0
+for match in matches:
+	start = match.start()
+	do_match = data.rfind("do()", 0, start)
+	dont_match = data.rfind("don't()", 0, start)
+	if do_match == dont_match == -1: # Neither
+		total += int(match.group(1)) * int(match.group(2))
+		continue
+	if do_match > dont_match: # Do
+		total += int(match.group(1)) * int(match.group(2))
+		continue
+	else: # Don't
+		continue
+print(total)
